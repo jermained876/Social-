62d61d638d8f988adf7303444e170e959b9c66f8
Your Name <you@example.com>
2020-01-08 19:12:26 -0400

    Model Relationship

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,26 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Model\Reply;
+use App\Model\Category;
 
 class Question extends Model
 {
-    //
+
+
+    public function user ()
+    {
+      return $this->belongsTo(User::class);
+    }
+
+    public function reply()
+  {
+    return $this->hasMany(Reply::class);
+  }
+  public function category()
+  {
+    return belongsTo(Category::class);
+  }
+
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Model;
+use App\Model\Question;
+use App\User;
+use App\Model\Like;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+      return $this->belongsTo(Question::class);
+    }
+
+    public function user ()
+    {
+      return $this->belongsTo(User::class);
+    }
+
+    public function like()
+    {
+      return $this->hasMany(Like::class);
+    }
+
 }
