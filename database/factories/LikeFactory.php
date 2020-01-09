5b2cea553126b489e5d6a4d1c7337cba3c679fa1
Your Name <you@example.com>
2020-01-08 20:23:27 -0400

    Factory and Seed

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -4,9 +4,14 @@
 
 use App\Model\Like;
 use Faker\Generator as Faker;
+use App\Model\Reply;
+use App\User;
 
 $factory->define(Like::class, function (Faker $faker) {
     return [
-        //
+        
+        'user_id' => function(){
+          return User::all()->random();
+        },
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+Use App\Model\Category;
+Use App\Model\Like;
+Use App\Model\Question;
+Use App\Model\Reply;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        factory(User::class,10)->create();
+        factory(Category::class,5)->create();
+        factory(Question::class,10)->create();
+        factory(Reply::class,50)->create()->each(function($reply){
+          factory(Like::class,1)->create([
+            'reply_id' => $reply->id,
+          ]);
+        });
+
     }
 }
